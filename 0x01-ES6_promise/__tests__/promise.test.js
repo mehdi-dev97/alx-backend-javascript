d284daf0f5c3d8da9203a4cb0109363dafab0b7f
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2024-03-06 15:39:25 +0000

    Return a Promise using this prototype function getResponseFromAPI()

---
+++ 0x01-ES6_promise/__tests__/promise.test.js
@@ -0,0 +1,7 @@
+import getResponseFromAPI from "../0-promise.js";
+
+const response = getResponseFromAPI();
+
+test('Return a Promise using this prototype getResponseFromAPI', () => {
+    expect(response instanceof Promise).toBe(true);
+});
