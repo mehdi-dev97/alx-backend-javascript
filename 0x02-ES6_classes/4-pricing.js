d204a9426466cafa2c70c4ed1e456b3e4d9b13f1
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2024-03-07 14:59:57 +0000

    Implement a class named Pricing with attributes[amount, currency instance] with getter, setter and displayFullPrice method to show full price description.

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,79 +1,79 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-    /**
+  /**
      * Create a new pricing @see {@link Pricing}.
      *
      * @param {Number} amount - amount of pricing.
      * @param {Currency} currency - currency instance.
      */
-    constructor (amount, currency) {
-        this.amount = amount;
-        this.currency = currency;
-    }
+  constructor (amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
-    /**
+  /**
      * Get amount of pricing.
-     * 
+     *
      * @returns {String}
      */
-    get amount () {
-        return this._amount;
-    }
+  get amount () {
+    return this._amount;
+  }
 
-    /**
+  /**
      * Set a new amount of pricing.
      */
-    set amount (value) {
-        if (typeof value !== 'number') {
-            throw new TypeError('Amount must be a number');
-        }
-        this._amount = value;
+  set amount (value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
     }
+    this._amount = value;
+  }
 
-    /**
+  /**
      * Get currency.
-     * 
+     *
      * @returns {Currency}
      */
-    get currency () {
-        return this._currency;
-    }
+  get currency () {
+    return this._currency;
+  }
 
-    /**
+  /**
      * Set a new currency of pricing.
      */
-    set currency (value) {
-        if (!(value instanceof Currency)) {
-            throw new TypeError('Currency must be a Currency');
-        }
-        this._currency = value;
+  set currency (value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
     }
+    this._currency = value;
+  }
 
-    /**
+  /**
      * Display full price description.
-     * 
+     *
      * @returns {String}
      */
-    displayFullPrice () {
-        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-    }
+  displayFullPrice () {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-    /**
+  /**
      * Display full price description.
-     * 
+     *
      * @param {Number} amount - amount pricing.
      * @param {Number} conversionRate - conversion rate pricing.
-     * 
+     *
      * @returns {Number}
      */
-    static convertPrice (amount, conversionRate) {
-        if (typeof amount !== 'number') {
-            throw new TypeError('Amount muste be a number');
-        }
-        if (typeof conversionRate !== 'number') {
-            throw new TypeError('Conversion Rate muste be a number');
-        }
-        return (amount * conversionRate);
+  static convertPrice (amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount muste be a number');
     }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion Rate muste be a number');
+    }
+    return (amount * conversionRate);
+  }
 }
