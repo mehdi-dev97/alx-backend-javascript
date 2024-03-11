cf947b768a6872afc4ccfd6318c33b8974971e48
Mehdi Ait Mouh <mehdi.ait.mouh.dev@gmaiL.com>
2024-03-11 17:16:23 +0000

    function updateStudentGradeByCity that returns an array of students for a specific city with their new grade.

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,19 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => {
+        return {
+          id: student.id,
+          firstName: student.firstName,
+          location: student.location,
+          grade:
+            newGrades
+              .filter((grade) => grade.studentId === student.id)
+              .reduce((acum, current) => acum.grade ?? current.grade, "N/A") ??
+            "N/A",
+        };
+      });
+  }
+  return [];
+}
