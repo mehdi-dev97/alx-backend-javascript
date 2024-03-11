732791c48e090d1dd81debefc5109590a35b0ccc
Mehdi Ait Mouh <mehdi.ait.mouh.dev@gmaiL.com>
2024-03-11 17:58:29 +0000

    function named setFromArray that returns a Set from an array.

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,13 @@
+/**
+ * Create set from an array.
+ *
+ * @param {Array} array
+ * @returns
+ */
+
+export default function setFromArray(array) {
+  if (!array instanceof Array) {
+    throw new Error("Argument most be an array");
+  }
+  return new Set(array);
+}
