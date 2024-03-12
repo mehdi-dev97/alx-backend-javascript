b7032318868e83b02707dacca6ae40621b36a90b
Mehdi Ait Mouh <mehdi.ait.mouh.dev@gmaiL.com>
2024-03-12 12:49:54 +0000

    Create a map of groceries.

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * Create a map of groceries.
+ *
+ * @returns {Map}
+ */
+
+export default function groceriesList () {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ]);
+}
