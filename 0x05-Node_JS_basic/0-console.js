8bf8841bdd0341ffd5b51761ff9ac7850198bbbc
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2024-04-29 16:26:02 -0800

    function named displayMessage that prints in STDOUT the string argument.

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a given message on the STDOUT.
+ * @param {String} message The message to display.
+ * @author Mehdi Ait Mouh <https://github.com/mehdi-dev97>
+ */
+function displayMessage(message) {
+    console.log(message);
+};
+
+module.exports = displayMessage;
